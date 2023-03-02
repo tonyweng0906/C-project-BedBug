9a0922f1d8b1c9c026d4c7d84cf5e8d2bd599b46
Neil Khedekar <64322243+titan-6@users.noreply.github.com>
2023-03-02 18:45:52 -0500

    Added Bug class

---
+++ GameLib/Bug.h
@@ -0,0 +1,50 @@
+/**
+ * @file Bug.h
+ * @author Team BedBug
+ *
+ *
+ */
+
+#include "GameObject.h"
+
+#ifndef PROJECT1BEDBUG_GAMELIB_BUG_H
+#define PROJECT1BEDBUG_GAMELIB_BUG_H
+
+/**
+ * Base class for a bug
+ * This applies to all of the bug
+ * object in the game.
+ */
+class Bug : public GameObject
+{
+private:
+	/// Bug speed in the X direction
+	/// in pixels per second
+	double mSpeedX;
+
+	/// Bug speed in the Y direction
+	/// in pixels per second
+	double mSpeedY;
+
+public:
+	/// Default constructor (disabled)
+	Bug() = delete;
+
+	/// Copy constructor (disabled)
+	Bug(const Bug &) = delete;
+
+	/// Assignment operator
+	void operator=(const Bug &) = delete;
+
+protected:
+	Bug(Game *game, const std::wstring &filename);
+
+	void Update(double elapsed);
+/**
+	wxXmlNode *XmlSave(wxXmlNode *node);
+	void XmlLoad(wxXmlNode *node);
+*/
+	void BugSpeed(double speedX, double speedY);
+};
+
+#endif //PROJECT1BEDBUG_GAMELIB_BUG_H
