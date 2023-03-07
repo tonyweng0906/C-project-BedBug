5ad253ee7bc0e8469dd34ad90cd3ae083bb66b4e
aron <aronlondond@gmail.com>
2023-03-07 14:42:34 -0500

    Added PlayArea class and initial functions and object collection Still need loading and saving functions

---
+++ GameLib/PlayArea.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file PlayArea.cpp
+ * @author Aron DuBois
+ */
+
+#include "pch.h"
+#include "PlayArea.h"
+
+/**
+ * Add an object to the PlayArea
+ * @param object new object to add
+ */
+void PlayArea::Add(std::shared_ptr<GameObject> object)
+{
+	mObjects.push_back(object);
+}
+
+
+/**
+ * Test an x,y click location to see if it clicked
+ * on some object in the play area
+ * @param x X location
+ * @param y Y location
+ * @return Pointer to object clicked on or nullptr if none.
+ */
+std::shared_ptr<GameObject> PlayArea::HitTest(int x, int y)
+{
+	for (auto i = mObjects.rbegin(); i != mObjects.rend(); i++)
+	{
+		if ((*i)->HitTest(x,y))
+		{
+			return *i;
+		}
+	}
+	return nullptr;
+}
+
+
+/**
+ * Delete an object from the play area
+ * @param object the object to delete
+ */
+void PlayArea::DeleteObject(std::shared_ptr<GameObject> object)
+{
+	auto loc = find(std::begin(mObjects), std::end(mObjects), object);
+	if (loc != std::end(mObjects))
+	{
+		mObjects.erase(loc);
+	}
+}
+
+/**
+ * Handle updates for animation
+ * @param elapsed The time since the last update
+ */
+void PlayArea::Update(double elapsed)
+{
+	for (auto object : mObjects)
+	{
+		object->Update(elapsed);
+	}
+
+}
+
+/**
+ * Accept a visitor for the collection
+ * @param visitor The visitor for the collection
+ */
+void PlayArea::Accept(ObjectVisitor *visitor)
+{
+	for (auto object : mObjects)
+	{
+		//Need to build accept function in object class
+		//object.Accept(visitor);
+	}
+}
---
+++ GameLib/PlayArea.h
@@ -0,0 +1,54 @@
+/**
+ * @file PlayArea.h
+ * @author Aron DuBois
+ *
+ * Class to hold game objects
+ */
+
+#ifndef PROJECT1BEDBUG_GAMELIB_PLAYAREA_H
+#define PROJECT1BEDBUG_GAMELIB_PLAYAREA_H
+
+#include <memory>
+#include <vector>
+#include "GameObject.h"
+
+
+// forward reference for object visitor
+class ObjectVisitor;
+
+/**
+ * Class to hold game objects
+ */
+class PlayArea
+{
+private:
+
+	/// All of the game objects that make up the game
+	std::vector<std::shared_ptr<GameObject> > mObjects;
+
+public:
+	PlayArea();
+
+	/**
+	 * Destructor
+	 */
+	 virtual ~PlayArea() = default;
+
+	 void Add(std::shared_ptr<GameObject> object);
+
+	 std::shared_ptr<GameObject> HitTest(int x, int y);
+
+	 void DeleteObject(std::shared_ptr<GameObject> object);
+
+	 void Update(double elapsed);
+
+	 void Accept(ObjectVisitor * visitor);
+
+	 void OnDraw(wxDC* graphics);
+
+	// Loading and saving functions go here?
+
+
+};
+
+#endif //PROJECT1BEDBUG_GAMELIB_PLAYAREA_H
