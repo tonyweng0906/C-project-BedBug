d59d305aaf4a0b08b926e9a2ea5285093c6ba82c
aron <aronlondond@gmail.com>
2023-03-22 17:26:32 -0400

    Removed Old hittest code from gameObject

--- GameLib/GameObject.cpp
+++ GameLib/GameObject.cpp
@@ -67,28 +67,6 @@ bool GameObject::HitTest(int x, int y)
 
 	return sqrt(dx * dx + dy * dy) < BugHitRange;
 
-	// Previous pre-build code
-
-//	double wid = mGameBitmap->GetWidth();
-//	double hit = mGameBitmap->GetHeight();
-//
-//	// Make x and y relative to the top-left corner of the bitmap image
-//	// Subtracting the center makes x, y relative to the image center
-//	// Adding half the size makes x, y relative to theimage top corner
-//	double testX = x - GetX() + wid / 2;
-//	double testY = y - GetY() + hit / 2;
-//
-//	// Test to see if x, y are in the image
-//	if (testX < 0 || testY < 0 || testX >= wid || testY >= hit)
-//	{
-//		// We are outside the image
-//		return false;
-//	}
-//
-//	// Test to see if x, y are in the drawn part of the image
-//	// If the location is transparent, we are not in the drawn
-//	// part of the image
-//	return !mGameImage->IsTransparent((int)testX, (int)testY);
 }
 
 void GameObject::Draw(std::shared_ptr<wxGraphicsContext> dc)
